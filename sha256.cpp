bb56800262d1aa9a126b14fd3176661a9415c48f
Max Kotlan <max.maxkot@gmail.com>
2021-05-06 10:40:00 -0400

    message queue wip

--- sha256.cpp
+++ sha256.cpp
@@ -24,6 +24,12 @@ T swap_endian(T u)
     return dest.u;
 }
 
+template<class T>
+T ror(T x, unsigned int moves)
+{
+    return (x >> moves) | (x << sizeof(T) * 8 - moves);
+}
+
 typedef std::vector<uint8_t> MessageBlock;
 
 const uint8_t messageBlockByteSize = 64;
@@ -34,38 +40,66 @@ void Print(std::vector<uint8_t>& input){
     std::cout << std::endl;
 }
 
-void copyFullMessageBlock(MessageBlock& m, std::vector<uint8_t> &input, uint64_t blockOffset) {
-    std::copy(input.begin(), input.end(), m.begin());
+uint32_t sigmaZero(uint32_t a) {
+    return (ror(ror(a, 7), 18) >> 3);
 }
 
-void copyPartialMessageBlock(MessageBlock& m, std::vector<uint8_t>& input) {
-    std::copy(input.begin(), input.end(), m.begin());
+void copyFullMessageBlock(MessageBlock& m, std::vector<uint8_t> &input, uint64_t blockOffset) {
+    const uint64_t beginning = blockOffset * messageBlockByteSize;
+    const uint64_t end = beginning + messageBlockByteSize;
+    std::copy(input.begin()+ beginning, input.begin() + end, m.begin());
+}
+
+void copyPartialMessageBlock(MessageBlock& mb, std::vector<uint8_t>& input, uint64_t blockOffset) {
+    const uint64_t beginning = blockOffset * messageBlockByteSize;
+    const uint64_t end = beginning + (input.size() % messageBlockByteSize);
+    std::copy(input.begin() + beginning, input.begin() + end, mb.begin());
     const uint64_t one = 0b10000000;
     uint64_t message_size = swap_endian(input.size());
-    std::memcpy(m.data() + input.size() % messageBlockByteSize, &one, 1);
-    std::memcpy(m.data() + messageBlockByteSize-sizeof(uint64_t), &message_size, sizeof(uint64_t));
+    std::memcpy(mb.data() + input.size() % messageBlockByteSize, &one, 1);
+    std::memcpy(mb.data() + messageBlockByteSize-sizeof(uint64_t), &message_size, sizeof(uint64_t));
 }
 
 std::vector<MessageBlock> CreateMessageBlocks(std::vector<uint8_t>& input){
-    uint64_t messageBlockCount = input.size()/64+1;
+    uint64_t messageBlockCount = (input.size()+9)/64+1;
     std::vector<MessageBlock> messageBlocks(messageBlockCount, MessageBlock(messageBlockByteSize));
     for (uint64_t i = 0; i < messageBlockCount; i++) {
 
         if ((i + 1) * messageBlockByteSize > input.size()) {
-            copyPartialMessageBlock(messageBlocks[i], input);
+            copyPartialMessageBlock(messageBlocks[i], input, i);
         } else {
             copyFullMessageBlock(messageBlocks[i], input, i);
         }
     }
     return messageBlocks;
 }
+#include <bitset>
 
 Hash Sha256(std::vector<uint8_t> input){
+
     std::vector<MessageBlock> messageBlocks = std::move(CreateMessageBlocks(input));
+
+    for (int i = 0; i < messageBlocks.size(); i++) {
+        uint32_t w[64];
+        for (int j = 0; j < 16; j++) {
+            std::memcpy(&w[j], messageBlocks[i].data() + j*sizeof(uint32_t), sizeof(uint32_t));
+            uint32_t endianswap = swap_endian(w[j]);
+            std::memcpy(&w[j], &endianswap, sizeof(uint32_t));
+            std::cout << std::hex << w[j] << std::endl;
+        }
+        for (int j = 16; j < 64; j++) {
+            w[j] = sigmaZero(w[j - 2]) + sigmaZero(w[j - 7]) + w[j - 16];
+            uint32_t endianswap = swap_endian(w[j]);
+            w[j] = endianswap;
+            std::cout << std::bitset<32>(w[j]) << std::endl;
+        }
+    }
+
     std::cout << "First Message Block: " << std::endl;
     Print(messageBlocks[0]);
     //Print(input);
     //Pad(input); 
+    std::cout << "lol" << std::endl;
     Print(input);
 
     Hash m(input);
--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(){
-    std::vector<uint8_t> input = {'a', 'b', 'c'};
+    std::vector<uint8_t> input = {'a','b','c'};
     Hash result = Sha256(input);
     std::cout << "Result: " << result.toString() << std::endl;
     assert(result.toString() == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
